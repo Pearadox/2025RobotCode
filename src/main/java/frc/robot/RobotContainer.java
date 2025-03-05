ec81d109c7ee5e846702aba8732a5ba341b8f2aa
Grace.ij <ji.ruiqi@gmail.com>
2025-03-04 19:04:42 -0600

    Drive forward, L2 auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,10 +9,12 @@ import static edu.wpi.first.units.Units.*;
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.Commands.ElevatorCommands;
@@ -45,9 +47,9 @@ public class RobotContainer {
     public final SendableChooser<Command> autoChooser;
 
     public RobotContainer() {
-        autoChooser = AutoBuilder.buildAutoChooser("Default");
+        autoChooser = AutoBuilder.buildAutoChooser("GH_L4");
         SmartDashboard.putData("Auto Mode", autoChooser);
-
+        registerNamedCommands();
         configureBindings();
     }
 
@@ -57,9 +59,10 @@ public class RobotContainer {
         drivetrain.setDefaultCommand(
                 // Drivetrain will execute this command periodically
                 drivetrain.applyRequest(
-                        () -> drive.withVelocityX(
-                                        -driverController.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
-                                .withVelocityY(-driverController.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+                        () -> drive.withVelocityX(-driverController.getLeftY()
+                                        * MaxSpeed) // Drive forward with negative Y (forward)
+                                .withVelocityY(
+                                        -driverController.getLeftX() * MaxSpeed) // Drive left with negative X (left)
                                 .withRotationalRate(-driverController.getRightX()
                                         * MaxAngularRate) // Drive counterclockwise with negative X (left)
                         ));
@@ -67,9 +70,10 @@ public class RobotContainer {
         elevator.setDefaultCommand(ElevatorCommands.setElevatorStowedMode(elevator));
 
         driverController.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        driverController.b()
-                .whileTrue(drivetrain.applyRequest(
-                        () -> point.withModuleDirection(new Rotation2d(-driverController.getLeftY(), -driverController.getLeftX()))));
+        driverController
+                .b()
+                .whileTrue(drivetrain.applyRequest(() -> point.withModuleDirection(
+                        new Rotation2d(-driverController.getLeftY(), -driverController.getLeftX()))));
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
@@ -92,5 +96,16 @@ public class RobotContainer {
 
     public Command getAutonomousCommand() {
         return autoChooser.getSelected();
+    }
+
+    public void registerNamedCommands() {
+        NamedCommands.registerCommand(
+                "ElevatorStationMode", new InstantCommand(() -> elevator.setElevatorStationMode()));
+        NamedCommands.registerCommand("ElevatorStowedMode", new InstantCommand(() -> elevator.setElevatorStowedMode()));
+        NamedCommands.registerCommand("ElevatorLevelTwo", new InstantCommand(() -> elevator.setElevatorLevelTwoMode()));
+        NamedCommands.registerCommand(
+                "ElevatorLevelThree", new InstantCommand(() -> elevator.setElevatorLevelThreeMode()));
+        NamedCommands.registerCommand(
+                "ElevatorLevelFour", new InstantCommand(() -> elevator.setElevatorLevelFourMode()));
     }
 }
