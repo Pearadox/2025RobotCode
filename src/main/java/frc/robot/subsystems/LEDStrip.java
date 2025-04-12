fcdb61c7f35d8f727025d004d133b45acd37713f
pearadox <iamawesome1234w@gmail.com>
2025-04-11 19:59:37 -0500

    Start led changes for Dynamo Demo

--- src/main/java/frc/robot/subsystems/LEDStrip.java
+++ src/main/java/frc/robot/subsystems/LEDStrip.java
@@ -1,5 +1,8 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.*;
+
+import edu.wpi.first.units.measure.Frequency;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -9,17 +12,24 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.LEDConstants;
+import frc.robot.RobotContainer;
 import java.util.function.BooleanSupplier;
 
 public class LEDStrip extends SubsystemBase {
     private AddressableLED led;
     private AddressableLEDBuffer ledBuffer;
+    // private AddressableLEDBufferView DynamoBufferViewLeft;
+    // private AddressableLEDBufferView DynamoBufferViewRight;
 
     private static final LEDStrip LEDSTRIP = new LEDStrip();
 
     public static LEDStrip getInstance() {
         return LEDSTRIP;
     }
+
+    private static final Color kRavenBlack = new Color("#101820");
+    private static final Color kDynamoOrange = new Color("#FF6A00");
+    private static final Color kSpaceCityBlue = new Color("#92C3F1");
 
     public LEDStrip() {
         led = new AddressableLED(LEDConstants.PORT);
@@ -31,7 +41,17 @@ public class LEDStrip extends SubsystemBase {
 
     public void periodic() {
         if (DriverStation.isDisabled()) {
-            setGradient(Color.kGreen, Color.kGold);
+            setGradient(LEDConstants.SCROLL_FREQ, kDynamoOrange, kDynamoOrange, kSpaceCityBlue);
+        } else {
+            setGradient(
+                    Percent.per(Second)
+                            .of(100
+                                    + (50
+                                            * Math.sqrt(Math.pow(RobotContainer.driverController.getLeftX(), 2)
+                                                    + Math.pow(RobotContainer.driverController.getLeftY(), 2)))),
+                    kDynamoOrange,
+                    kDynamoOrange,
+                    kSpaceCityBlue);
         }
         led.setData(ledBuffer);
     }
@@ -63,6 +83,13 @@ public class LEDStrip extends SubsystemBase {
     private void setGradient(Color... colors) {
         LEDPattern gradient = LEDPattern.gradient(LEDPattern.GradientType.kContinuous, colors)
                 .scrollAtRelativeSpeed(LEDConstants.SCROLL_FREQ);
+        gradient.applyTo(ledBuffer);
+        led.setData(ledBuffer);
+    }
+
+    private void setGradient(Frequency frequency, Color... colors) {
+        LEDPattern gradient =
+                LEDPattern.gradient(LEDPattern.GradientType.kContinuous, colors).scrollAtRelativeSpeed(frequency);
         gradient.applyTo(ledBuffer);
         led.setData(ledBuffer);
     }
