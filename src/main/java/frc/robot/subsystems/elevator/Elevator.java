a91cd4077eb39ad874a3ec045b55a5b76e1bd00f
pearadox <iamawesome1234w@gmail.com>
2025-10-10 11:55:03 -0500

    Swap default elevator offset after first practice match

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -16,7 +16,7 @@ import org.littletonrobotics.junction.Logger;
 public class Elevator extends SubsystemBase {
     // private InverseKinematics ik = new InverseKinematics();
 
-    private double elevatorOffset = Constants.IDENTITY == RobotIdentity.EVE ? 0.6814 : -0.3;
+    private double elevatorOffset = Constants.IDENTITY == RobotIdentity.EVE ? -0.3 : 0.61;
 
     private boolean isCoral = true;
     private boolean isAligning = false;
